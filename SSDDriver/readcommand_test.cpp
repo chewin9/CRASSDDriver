0a00f3f5aa869f03d78bb412132443439383885d
junslee0912 <junslee0912@gmail.com>
2025-07-29 16:06:40 +0900

    [Testing] Add Testcase

--- SSDDriver/readcommand_test.cpp
+++ SSDDriver/readcommand_test.cpp
@@ -1,31 +1,42 @@
 #include "readcommand.h"
-#include "gmock/gmock.h"
+
 #include <string>
+
+#include "gmock/gmock.h"
 
 TEST(ReadCommand, NormalRead) {
   ReadCommand rc;
   int normalLba = 10;
   std::string normalValue = "0xFFFFFFFF";
-  EXPECT_TRUE(rc.Read(normalLba, normalValue));
+  for (int i = 0; i < 100; i++) {
+    EXPECT_TRUE(rc.Read(normalLba, normalValue));
+  }
 }
 
-TEST(ReadCommand, ReadWithAbnormalLba) { 
-	ReadCommand rc;
+TEST(ReadCommand, ReadWithAbnormalLba) {
+  ReadCommand rc;
   int abnormalLba = -1;
-   std::string normalValue = "0xFFFFFFFF";
-  EXPECT_FALSE(abnormalLba, normalValue);
+  std::string normalValue = "0xFFFFFFFF";
+  EXPECT_FALSE(rc.Read(abnormalLba, normalValue));
 }
 
 TEST(ReadCommand, ReadWithAbnormalValue_NoFullWords) {
   ReadCommand rc;
-  int abnormalLba = 1;
-  std::string normalValue = "0xFFF";
-  EXPECT_FALSE(abnormalLba, normalValue);
+  int normalLba = 1;
+  std::string abnormalValue = "0xFFF";
+  EXPECT_FALSE(rc.Read(normalLba, abnormalValue));
 }
 
 TEST(ReadCommand, ReadWithAbnormalValue_NoHexadecimalVal) {
   ReadCommand rc;
-  int abnormalLba = 1;
-  std::string normalValue = "0x00000GGG";
-  EXPECT_FALSE(abnormalLba, normalValue);
+  int normalLba = 1;
+  std::string abnormalValue = "0x00000GGG";
+  EXPECT_FALSE(rc.Read(normalLba, abnormalValue));
+}
+
+TEST(ReadCommand, ReadWithAbnormalValue_NoStartWith0x) {
+  ReadCommand rc;
+  int normalLba = 1;
+  std::string abnormalValue = "FFFFFFFF";
+  EXPECT_FALSE(rc.Read(normalLba, abnormalValue));
 }
