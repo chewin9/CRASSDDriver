25708fb909caa38834d35de241e4945c4e893a2e
yeongi.jin <jinygg78@gmail.com>
2025-07-29 16:24:15 +0900

    [fix] to inject process excuter

--- TestShell/testscript_test.cpp
+++ TestShell/testscript_test.cpp
@@ -1,5 +1,12 @@
 #include "gmock/gmock.h"
+#include "testscript.h"
 
-TEST(shell_script, 1_FullWriteAndReadCompare) {
-	EXPECT_EQ(1, 1);
+class SSDMock : public IProcessExecutor {
+public:
+	MOCK_METHOD((int), Process, (const std::string&), (override));
+};
+
+TEST(testscript, 1_FullWriteAndReadCompare) {
+	SSDMock mock;
+	EXPECT_CALL(mock, Process("copy")).Times(1);
 }
