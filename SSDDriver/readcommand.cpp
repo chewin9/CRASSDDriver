c924473040f6e848ccddc5c7c3f857dbc1240e3a
junslee0912 <junslee0912@gmail.com>
2025-07-29 16:16:45 +0900

    [Green] Add Production Code

--- SSDDriver/readcommand.cpp
+++ SSDDriver/readcommand.cpp
@@ -1,3 +1,15 @@
 #include "readcommand.h"
 
-bool ReadCommand::Read(int lba, std::string val) { return true; }
+bool ReadCommand::Read(int lba, std::string val) { 
+	if (lba < 0 || lba > 99) return false;
+  if (val == "" || val.size() != 10 ||
+      (val.size() > 2 && val.substr(0, 2) != "0x"))
+    return false;
+  for (int i = 2; i < 10; ++i) {
+    char c = val[i];
+    if (!isdigit(c) && !(c >= 'A' && c <= 'F')) {
+      return false;
+    }
+  }
+	return true; 
+}
