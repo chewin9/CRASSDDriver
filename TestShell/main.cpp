8c4058ac78a61e1e8eb0368ecd29dcc80bb3b3bb
inhae.kang <kang.inhae@gmail.com>
2025-07-30 15:02:07 +0900

    [fix] User Debug/Release option for choosing main function

--- TestShell/main.cpp
+++ TestShell/main.cpp
@@ -1,16 +1,12 @@
 #include <gmock/gmock.h>
 #include "shell.h"
 
-#define UNIT_TEST (0)
-
-#if (UNIT_TEST)
 int main(void) {
+#if (_DEBUG)
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
-}
 #else 
-int main(void) {
 	Shell shell;
 	return shell.Run();
+#endif 
 }
-#endif
