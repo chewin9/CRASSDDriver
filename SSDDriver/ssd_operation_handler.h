eccf74ac9422ced3064b88a6964648e0fc092d91
junslee0912 <junslee0912@gmail.com>
2025-07-30 18:03:40 +0900

    Add ssd_operation_handler class

---
+++ SSDDriver/ssd_operation_handler.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include "file_io.h"
+class SsdOperationHandler {
+ private:
+  std::vector<std::pair<int, std::string>> nandData;
+  FileIO& fileHandler;
+  ParsedCommand cmdInfo;
+
+ public:
+  SsdOperationHandler(FileIO& handler, ParsedCommand& cmdInfo)
+      : fileHandler{handler}, cmdInfo{cmdInfo} {}
+
+  void write();
+
+  bool read();
+};
