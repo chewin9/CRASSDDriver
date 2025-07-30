4bb424951daa3739aa002c87210262d3495e38fe
junslee0912 <junslee0912@gmail.com>
2025-07-30 10:01:24 +0900

    [Green/Refactoring] Add CommandParser class

---
+++ SSDDriver/command_parser.cpp
@@ -0,0 +1,51 @@
+#include "command_parser.h"
+
+#include <stdexcept>
+
+void CommandParser::CheckException(int argc, char* argv[]) {
+  if (argc < 3) {
+    throw std::invalid_argument("Not Enough Arguments!");
+  }
+  if (argc == 3) {
+    if (argv[1] == "W") throw std::invalid_argument("Not Enough Arguments!");
+  }
+  if (argc == 4) {
+    if (argv[1] == "R") throw std::invalid_argument("Not Enough Arguments!");
+  }
+}
+
+bool CommandParser::IsLbaErrorExist(int argc, char* argv[]) {
+  if (cmd.lba < MIN_VAL || cmd.lba > MAX_VAL) {
+    return true;
+  }
+
+  return false;
+}
+
+bool CommandParser::IsValueErrorExist(int argc, char* argv[]) {
+  if (cmd.opCode == "R") return false;
+  if (cmd.value == "" || cmd.value.size() != SIZE_OF_VALUE ||
+      (cmd.value.size() > SIZE_OF_HEX_NOTATION &&
+       cmd.value.substr(0, SIZE_OF_HEX_NOTATION) != "0x")) {
+    return true;
+  }
+
+  for (int i = SIZE_OF_HEX_NOTATION; i < SIZE_OF_VALUE; ++i) {
+    char c = cmd.value[i];
+    if (!isdigit(c) && !(c >= 'A' && c <= 'F')) {
+      return true;
+    }
+  }
+  return false;
+}
+
+ParsedCommand CommandParser::ParseCommand(int argc, char* argv[]) {
+  CheckException(argc, argv);
+  cmd.opCode = argv[1];
+  cmd.lba = std::stoi(argv[2]);
+  if (cmd.opCode == "W") cmd.value = argv[3];
+  if (IsLbaErrorExist(argc, argv) || IsValueErrorExist(argc, argv)) {
+    cmd.errorFlag = true;
+  }
+  return cmd;
+}
--- SSDDriver/readcommand_test.cpp
+++ SSDDriver/readcommand_test.cpp
@@ -10,27 +10,12 @@ using namespace testing;
 class ReadCommandFixture : public Test {
  public:
   ReadCommand rc;
-  int normalLba = 1;
-  int abnormalLba = -1;
   const int MAX_VAL_SIZE = 100;
   std::string normalValue = "0xFFFFFFFF";
-  std::vector<std::string> abnormalValue = {"0xFFF", "0x00000GGG", "FFFFFFFF", ""};
 };
 
 TEST_F(ReadCommandFixture, NormalRead) {
-  for (int i = 0; i < MAX_VAL_SIZE; i++) {
+  for (int normalLba = 0; normalLba < MAX_VAL_SIZE; normalLba++) {
     EXPECT_TRUE(rc.Execute(normalLba, normalValue));
-  }
-}
-
-TEST_F(ReadCommandFixture, ReadWithAbnormalLba) {
-  for (int i = 0; i < abnormalValue.size(); i++) {
-    EXPECT_FALSE(rc.Execute(normalLba, abnormalValue[i]));
-  }
-}
-
-TEST_F(ReadCommandFixture, ReadWithAbnormalLbaAndAbnormalValue) {
-  for (int i = 0; i < abnormalValue.size(); i++) {
-    EXPECT_FALSE(rc.Execute(abnormalLba, abnormalValue[i]));
   }
 }
