84b34561221457832862cb6baa9340c53ead83d5
inhae.kang <kang.inhae@gmail.com>
2025-07-30 17:40:31 +0900

    [refactor] clean up shell_test.cpp

--- TestShell/shell_test.cpp
+++ TestShell/shell_test.cpp
@@ -11,35 +11,43 @@ public:
 	MOCK_METHOD(string, get_command, (string& input), (override));
 };
 
-TEST(ShellTest, CallCommandWrite) {
-	MockProcessExecutor mock_exe;
-	MockShell mockshell(&mock_exe);
+class ShellTestFixture : public Test{
+public:
+	void SetUp() override {
+		mockshell = new MockShell(&mock_exe);
+	}
 
-	EXPECT_CALL(mockshell, get_command).WillOnce([](string& input) { input = "write 3 0xAAAABBBB";  return "write"; })
+	void TearDown() override {
+		delete mockshell;
+	}
+
+	MockProcessExecutor mock_exe;
+	MockShell* mockshell;
+};
+
+TEST_F(ShellTestFixture, CallCommandWrite) {
+
+	EXPECT_CALL(*mockshell, get_command).WillOnce([](string& input) { input = "write 3 0xAAAABBBB";  return "write"; })
 									   .WillRepeatedly([](string& input) { return "exit"; });
 	EXPECT_CALL(mock_exe, Process("SSDDriver.exe W 3 0xAAAABBBB")).Times(1);
 
-	mockshell.Run();
+	mockshell->Run();
 }
 
-TEST(ShellTest, CallCommandWriteFull) {
-	MockProcessExecutor mock_exe;
-	MockShell mockshell(&mock_exe);
+TEST_F(ShellTestFixture, CallCommandWriteFull) {
 
-	EXPECT_CALL(mockshell, get_command).WillOnce([](string& input) { input = "fullwrite 0xAAAABBBB";  return "fullwrite"; })
+	EXPECT_CALL(*mockshell, get_command).WillOnce([](string& input) { input = "fullwrite 0xAAAABBBB";  return "fullwrite"; })
 		.WillRepeatedly([](string& input) { return "exit"; });
 	EXPECT_CALL(mock_exe, Process).Times(100);
 
-	mockshell.Run();
+	mockshell->Run();
 }
 
-TEST(ShellTest, CallCommandRead) {
-	MockProcessExecutor mock_exe;
-	MockShell mockshell(&mock_exe);
+TEST_F(ShellTestFixture, CallCommandRead) {
 
-	EXPECT_CALL(mockshell, get_command).WillOnce([](string& input) { input = "read 3";  return "read"; })
+	EXPECT_CALL(*mockshell, get_command).WillOnce([](string& input) { input = "read 3";  return "read"; })
 		.WillRepeatedly([](string& input) { return "exit"; });
 	EXPECT_CALL(mock_exe, Process("SSDDriver.exe R 3")).Times(1);
 
-	mockshell.Run();
+	mockshell->Run();
 }
