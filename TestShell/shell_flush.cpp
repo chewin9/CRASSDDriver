7ea7e3cf6c55b1e39f4eff61b29ae2d260f680ad
inhae.kang <kang.inhae@gmail.com>
2025-07-31 17:27:11 +0900

    [feature] add flush funtion test and file

---
+++ TestShell/shell_flush.cpp
@@ -0,0 +1,31 @@
+#include "gmock/gmock.h"
+#include "test.h"
+#include "ishell_command.h"
+
+class ShellFlush : public IShellCommand {
+public:
+	ShellFlush(IProcessExecutor* executor) : executor_(executor) {}
+	bool Run(const std::string& cmd) override;
+private:
+	IProcessExecutor* executor_; 
+};
+
+TEST(FlushTest, TC1) {
+	MockProcessExecutor executor;
+	ShellFlush flush(&executor);
+	EXPECT_CALL(executor, Process("F")).Times(1);
+
+	flush.Run("flush");
+}
+
+bool ShellFlush::Run(const std::string& cmd) {
+	
+	// check parameter valid 
+	if (cmd != "flush") {
+		std::cout << "INVALID COMMAND" << std::endl;
+		return false;
+	}
+
+	executor_->Process("F");
+	return true;
+}
