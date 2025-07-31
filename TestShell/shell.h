09c400a50942a8ffcf56d0b9d3b0a62a52bd5db8
seungcheol.Lee <cheein9@gmail.com>
2025-07-31 17:15:34 +0900

    [feat] Add erase to shell
    Signed-off-by: seungcheol.Lee <cheein9@gmail.com>

--- TestShell/shell.h
+++ TestShell/shell.h
@@ -4,6 +4,8 @@
 #include "shell_write.h"
 #include "shell_full_write.h"
 #include "shell_full_read.h"
+#include "shell_erase.h"
+#include "shell_erase_range.h"
 #include "testscript.h"
 #include "shell_help.h"
 #include "process_executor.h"
