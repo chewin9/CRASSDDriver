db75d90351550387e0d18a398876cae37c2fb111
yaesolKim <kimyaesol@gmail.com>
2025-07-31 15:58:32 +0900

    add file generate/delete function

--- SSDDriver/file_io.h
+++ SSDDriver/file_io.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <unordered_map>
+#include <filesystem>
 #pragma once
 
 class FileIO {
@@ -7,8 +8,13 @@ class FileIO {
   std::unordered_map<int, std::string> LoadDataFromInput();
   void WriteValueToOutputFile(std::string str);
   void SaveData(std::unordered_map<int, std::string> entries);
+  void GenFolderAndEmtyFiles();
+  void EraseFolder();
 
  private:
   const std::string SSD_NAND_FILE = "ssd_nand.txt";
   const std::string SSD_OUTPUT_FILE = "ssd_output.txt";
+
+  const std::string SSD_COMMAND_BUFFER_FOLDER = "buffer";
+
 };
