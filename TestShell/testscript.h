70fa6688b2c3b209ffd9df38fee8d26ee214192c
jinygg <jinygg78@gmail.com>
2025-07-31 10:43:12 +0900

    [refactoring] adapt testscriptfactory in scriptrunner

--- TestShell/testscript.h
+++ TestShell/testscript.h
@@ -29,41 +29,6 @@ protected:
 	const std::string SSD_NAME = "SSDDriver.exe";
 };
 
-class DummyScript : public TestScript {
-public:
-	DummyScript(std::string name) : TestScript(name) {}
-	bool Run(IProcessExecutor* exe) override {
-		//Script
-		return false;
-	}
-};
-
-class FullWriteAndReadCompare : public TestScript {
-public:
-	FullWriteAndReadCompare(std::string name) : TestScript(name) {}
-	bool Run(IProcessExecutor* exe) override;
-};
-
-class PartialLBAWrite : public TestScript {
-public:
-	PartialLBAWrite(std::string name) : TestScript(name) {}
-	bool Run(IProcessExecutor* exe) override;
-	std::vector<std::string> value_list = { "0x1", "0x2","0x3","0x4","0x5" };
-
-private:
-	const unsigned int MAX_LOOP_COUNT = 30;
-	const unsigned int MAX_TEST_AREA = 5;
-
-	bool GetPartialReadAndCompareResult(IProcessExecutor* exe);
-	void PartialBlockWrite(IProcessExecutor* exe);
-};
-
-class WriteReadAging : public TestScript {
-public:
-	WriteReadAging(std::string name) : TestScript(name) {}
-	bool Run(IProcessExecutor* exe) override;
-};
-
 class TestScriptRunner {
 public:
 	TestScriptRunner(IProcessExecutor* exe);
---
+++ TestShell/testscriptfactory.cpp
@@ -0,0 +1,88 @@
+#include "testscriptfactory.h"
+#include <string>
+#include <exception>
+#include <iostream>
+#include <random>
+#include <ctime>
+#include "File.h"
+
+bool FullWriteAndReadCompare::Run(IProcessExecutor* exe) {
+	//Script
+	int value = 5;
+	int start = 0;
+	const int length = 5;
+
+	for (start = 0; start < MAX_ADDR; start += length) {
+		for (int index = start;index < start + length; index++) {
+			WriteBlock(exe, start, length, value);
+			if (ReadCompare(exe, start, length, value) == false) return false;
+		}
+	}
+
+	return true;
+}
+
+bool PartialLBAWrite::Run(IProcessExecutor* exe)
+{
+	bool IsPass = true;
+
+	for (int loopcount = 0; loopcount < MAX_LOOP_COUNT; loopcount++)
+	{
+		PartialBlockWrite(exe);
+
+		IsPass = GetPartialReadAndCompareResult(exe);
+
+		if (IsPass == false) break;
+	}
+
+	return IsPass;
+}
+
+bool PartialLBAWrite::GetPartialReadAndCompareResult(IProcessExecutor* exe)
+{
+	bool IsPass = true;
+
+	for (int readcount = 0; readcount < MAX_TEST_AREA; ++readcount) {
+		exe->Process(makeReadCommand(readcount));
+		try {
+			IsPass = IsPass && (stoi(File::ReadOutputFile().substr(2, 10)) == std::stoul(value_list[readcount], nullptr, 16));
+		}
+		catch (std::exception e) {
+			return false;
+		}
+	}
+
+	return IsPass;
+}
+
+void PartialLBAWrite::PartialBlockWrite(IProcessExecutor* exe)
+{
+	for (int writecount = 0; writecount < MAX_TEST_AREA; writecount++)
+	{
+		exe->Process(makeWriteCommand(writecount, std::stoul(value_list[writecount], nullptr, 16)));
+	}
+}
+
+bool WriteReadAging::Run(IProcessExecutor* exe) {
+	//Script
+	std::srand(std::time({}));
+	unsigned int data = rand();
+
+	for (int count = 0; count < 200; count++) {
+
+		WriteBlock(exe, 0, 1, data);
+		if (ReadCompare(exe, 0, 1, data) == false) return false;
+
+		WriteBlock(exe, 99, 1, data);
+		if (ReadCompare(exe, 99, 1, data) == false) return false;
+	}
+
+	return true;
+}
+
+TestScript* TestScriptFactory::createTestScript(std::string scriptname) {
+	if (scriptname == "0_Dummy") return new DummyScript(scriptname);
+	if (scriptname == "1_FullWriteAndReadCompare") return new FullWriteAndReadCompare(scriptname);
+	if (scriptname == "2_PartialLBAWrite") return new PartialLBAWrite(scriptname);
+	if (scriptname == "3_WriteReadAging") return new WriteReadAging(scriptname);
+}
