5a9fe4412a39c94c8c84d1736cd7ad2c3d2ba9bb
yaesolKim <kimyaesol@gmail.com>
2025-08-01 16:30:32 +0900

    add file io interface to use mock

--- SSDDriver/file_io.h
+++ SSDDriver/file_io.h
@@ -1,22 +1,20 @@
-#include <filesystem>
-#include <string>
-#include <unordered_map>
 #pragma once
 
-class FileIO {
- public:
-  std::unordered_map<int, std::string> LoadDataFromInput();
-  void WriteValueToOutputFile(std::string str);
-  void SaveData(std::unordered_map<int, std::string> entries);
-  void InitBufferDir();
-  void EraseBufferDir();
-  void ChangeFileName(std::vector<std::string>& in_command);
-  std::vector<std::string> getCommandBuffer();
-  
+#include <filesystem>
+#include <string>
+#include <unordered_map>"
+#include "file_io_interface.h"
 
- private:
-  std::vector<std::string> getFileNames();
-  const std::string SSD_NAND_FILE = "ssd_nand.txt";
-  const std::string SSD_OUTPUT_FILE = "ssd_output.txt";
-  const std::string SSD_COMMAND_BUFFER_FOLDER = "buffer";
+class FileIO : public FileIOInterface {
+public:
+    std::unordered_map<int, std::string> LoadDataFromInput() override;
+    void WriteValueToOutputFile(const std::string& str) override;
+    void SaveData(const std::unordered_map<int, std::string>& entries) override;
+    void InitBufferDir() override;
+    void EraseBufferDir() override;
+    void ChangeFileName(const std::vector<std::string>& in_command) override;
+    std::vector<std::string> getCommandBuffer() override;
+
+private:
+    std::vector<std::string> getFileNames();
 };
