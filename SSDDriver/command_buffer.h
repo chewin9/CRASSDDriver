1a56aa25c5866d778d31339e96eb0b553d4059b4
junslee0912 <junslee0912@gmail.com>
2025-08-01 09:31:34 +0900

    [Green] Add func

--- SSDDriver/command_buffer.h
+++ SSDDriver/command_buffer.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "command_parser.h"
+#include "file_io.h"
 #include <string>
 #include <unordered_map>
 #include <list>
 class CommandBuffer {
  private:
+  FileIO& fileio;
   std::vector<std::string> ConvertParsedCommandToStringList(const std::list<ParsedCommand>& cmdList);
   std::list<ParsedCommand> ParsingStringtoBuf(std::vector<std::string>& bufferList);
   void OptimizeBuffer(std::list<ParsedCommand>& bufferList,const ParsedCommand& cmd);
  public:
   void AddBuffer(const ParsedCommand& cmdInfo);
   std::string ReadBuffer(const ParsedCommand& cmdInfo);
-
-  std::unordered_map<int, std::string> GetBufferToFlush();
+  bool IsFlushNeeded();
+  std::list<ParsedCommand> GetCommandBuffer();
 };
