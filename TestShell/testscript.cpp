7454dcf7d6708b96c35df2c3ad5229ffe200addd
jinygg <jinygg78@gmail.com>
2025-08-01 13:12:03 +0900

    [bugfix] fix exception in reading output file

--- TestShell/testscript.cpp
+++ TestShell/testscript.cpp
@@ -88,7 +88,7 @@ void TestScript::EraseBlock(IProcessExecutor* exe, unsigned int startaddr, unsig
 	}
 }
 
-bool TestScript::ReadCompare(IProcessExecutor* exe, IFile* file, unsigned int startaddr, unsigned int len, unsigned value) {
+bool TestScript::ReadCompare(IProcessExecutor* exe, IFile* file, unsigned int startaddr, unsigned int len, unsigned int value) {
 	char buffer[100]; 
 	const int outStartPos = 2;
 	const int outEndPos = 10;
@@ -96,12 +96,15 @@ bool TestScript::ReadCompare(IProcessExecutor* exe, IFile* file, unsigned int st
 	std::snprintf(buffer, 100, "%s : %d length : %d, value : %08X", __func__, startaddr, len, value);
 	std::string str(buffer);
 
+	std::string outputfile;
+
 	PRINT(str);
 
 	for (unsigned int index = startaddr; index < startaddr + len; index++) {
 		exe->Process(makeReadCommand(index));
 		try {
-			if (std::stoi(file->ReadOutputFile("ssd_output.txt").substr(outStartPos, outEndPos), nullptr, HEX) != value) {
+			outputfile = file->ReadOutputFile("ssd_output.txt");
+			if (std::stoul(outputfile.substr(outStartPos, outEndPos), nullptr, HEX) != value) {
 				PRINT("Read result is mismatched :");
 				return false;
 			}
